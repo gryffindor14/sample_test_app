e7df4a3152a9318f766ed3a08fb7b1e9da155779
gryffindor14 <gryffindor14@gmail.com>
2011-10-28 14:55:08 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ SampleApp::Application.routes.draw do
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
+  
+  match '/contact', :to => 'pages#contact'
+	match '/about', :to => 'pages#about'
+	match '/help', :to => 'pages#help'
+	match '/', :to => 'pages#home/'
+	
+	root :to => 'pages#home'
+	root_path => '/'
+	root_url => 'http://localhost:3000/'
+
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
